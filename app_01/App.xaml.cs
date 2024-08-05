3614823a53bbe44ef7d316308bc2f97666be6045
Antunes Dos Santos <leonardo-antunes-dos-santos@siemens.com>
2024-08-05 15:25:57 -0400

    app01/initial developments

---
+++ app_01/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace app_01
+{
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ app_01/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace app_01
+{
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "PDF Source Fles | *.pdf";
+            ofd.InitialDirectory = "C:\\temp";
+            ofd.Title = "Please select PDF file(s).";
+            ofd.Multiselect = true;
+
+
+            bool? success = ofd.ShowDialog();
+            if (success == true)
+            {
+                string path = ofd.FileName;
+                string filename = ofd.SafeFileName;
+                tbResult.Text = filename + "  --  " + path;
+            }
+            else
+            {
+                //pass
+            }
+        }
+    }
+}
